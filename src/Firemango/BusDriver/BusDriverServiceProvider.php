7e58b141fed096c4b2bb009dd0087c1759c53b03
Kasper Bentsen <kasper@firemango.dk>
2014-06-10 17:44:10 -0500

    Initial Commit

---
+++ src/Firemango/BusDriver/BusDriverServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Firemango\BusDriver;
+
+use Illuminate\Support\ServiceProvider;
+
+class BusDriverServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('firemango/bus-driver');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
